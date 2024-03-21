283c552cb626a84e587a180094c20a186c36fa47
Vivek Bhanuprakash <vivek.bhanuprakash@gmail.com>
2024-03-21 12:52:14 -0700

    fixed redirect

--- server/internal/services/authentication/google.go
+++ server/internal/services/authentication/google.go
@@ -120,7 +120,7 @@ func (a *Authenticator) handleCallback(c *gin.Context) {
 		c.SetCookie("X-USER-TYPE", "INDIVIDUAL", 1200, "/", "localhost", false, false)
 	}
 
-	c.Redirect(http.StatusFound, "/app/index.html")
+	c.Redirect(http.StatusFound, "/app/home")
 }
 
 // getUserInfo fetches user information using the OAuth 2.0 token.
