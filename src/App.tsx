aefc1d397689271cf9f1251f027c43a185d3326c
Vivek Bhanuprakash <vivek.bhanuprakash@gmail.com>
2024-04-01 12:35:15 -0700

    reduced max screen width

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import './App.css';
-import SPPage from './sp-page';
-import BenPage from './ben-page';
 import { CookiesProvider, withCookies } from "react-cookie";
-
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import BenPage from "./ben-page";
+import SPPage from "./sp-page";
 
 function App() {
   return (
     <CookiesProvider>
-      <div className="max-w-screen-xl mx-auto mb-10">
+      <div className="mx-auto mb-10 max-w-screen-lg">
         <Router>
           <div>
             <nav className="bg-gray-800 p-4">
-              <ul className="flex justify-startitems-center">
+              <ul className="justify-startitems-center flex">
                 <li>
-                  <Link to="/app/sp" className="px-5 text-white hover:text-gray-300">Service Provider Demo</Link>
+                  <Link to="/app/sp" className="px-5 text-white hover:text-gray-300">
+                    Service Provider Demo
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/app/ben" className="px-5 text-white hover:text-gray-300">Beneficiary Demo</Link>
+                  <Link to="/app/ben" className="px-5 text-white hover:text-gray-300">
+                    Beneficiary Demo
+                  </Link>
                 </li>
               </ul>
             </nav>
